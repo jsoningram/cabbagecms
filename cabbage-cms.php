dc51474507806bc416eaba184ea5d4a608040cb1
jsoningram@gmail.com <jasoningram@Jasons-MacBook-Pro.local>
2015-09-02 17:58:09 -0700

    new admin bar colors for 4.3

--- cabbage-cms.php
+++ cabbage-cms.php
@@ -3,7 +3,7 @@
 Plugin Name: CabbageCMS
 Plugin URI: https://github.com/jsoningram/cabbagecms
 Description: Custom settings for More Cabbage CMS
-Version: 1.1.0
+Version: 1.1.1
 Author: Jason Ingram
 Author URI: http://morecabbage.com
 License: GPL2
